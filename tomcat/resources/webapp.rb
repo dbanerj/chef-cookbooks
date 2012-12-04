3dfeca514f4944941aff6ab3a0367db7f629e942
Stein Welberg <stein@innovation-district.com>
2012-12-04 13:13:20 +0100

    Remove default values for proxyPort, scheme and protocol settings in server.xml. Downsize nightly default memory settings

--- tomcat/resources/webapp.rb
+++ tomcat/resources/webapp.rb
@@ -5,14 +5,14 @@ attribute :user, :kind_of => String
 attribute :group, :kind_of => String
 attribute :http_port, :kind_of => Integer
 attribute :control_port, :kind_of => Integer
-attribute :secure, :kind_of => String, :default => "false"
-attribute :scheme, :kind_of => String, :default => "http"
-attribute :proxy_port, :kind_of => Integer, :default => 8080
+attribute :secure, :kind_of => String
+attribute :scheme, :kind_of => String
+attribute :proxy_port, :kind_of => Integer
 attribute :jdk_home, :kind_of => String
 
 attribute :war, :kind_of => String
 
 # Default memory settings per environment
-attribute :java_opts_nightly, :kind_of => String, :default => "-XX:MaxPermSize=256m -Xmx1024m"
+attribute :java_opts_nightly, :kind_of => String, :default => "-XX:MaxPermSize=128m -Xmx512m"
 attribute :java_opts_acc, :kind_of => String, :default => "-XX:MaxPermSize=256m -Xmx1024m"
 attribute :java_opts_prod, :kind_of => String, :default => "-XX:MaxPermSize=256m -Xmx2048m"
